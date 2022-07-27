736f7877cddedc150b1c6639f3364adffd41cf96
JUNG\alswj <20212856@office.deu.ac.kr>
2022-07-27 16:44:19 +0900

    메인 gui, 회원 gui 추가

---
+++ GUI/userGUI.py
@@ -0,0 +1,25 @@
+from cProfile import label
+from email.mime import image
+from tkinter import *
+import tkinter.font as tkFont
+import tkinter as tk
+
+main = Tk()
+main.title("Library")
+main.geometry("472x766+0+0")
+main.resizable(False,False)
+main.configure(bg='white')
+
+frame = tk.Frame(main)
+frame.pack()
+
+Lib_image = PhotoImage(file ='C:\\Users\\alswj\\OneDrive\\문서\\Library_management_ver2\\GUI\\program_image\\라이브러리로고.png')
+Lib_label = tk.Label(frame,image=Lib_image, bg='white')
+Lib_label.pack(anchor=NW, padx=20)
+
+label2 = tk.Label(frame,image=Lib_image)
+label2.place(x = 30, y = 40)
+
+Entry2 = Entry(main)
+Entry2.pack()
+main.mainloop()
